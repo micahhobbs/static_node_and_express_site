5ac2e63beb2dfb17c97f3ce48c4e439518e6d4e5
Micah Hobbs <micahthobbs@gmail.com>
2019-06-19 20:28:31 +1000

    Setting up express server and basic routes

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const express = require(`express`);
-const data = require(`./data.json`);
+// const data = require(`./data.json`);
+
+const app = express();
+app.use(express.static(`public`));
+
+app.set('view engine', 'pug');
+
+app.get(`/`, (req, res) => {
+  res.render(`index`);
+});
+
+app.get(`/about`, (req, res) => {
+  res.render(`about`);
+});
+
+app.listen(3000);
