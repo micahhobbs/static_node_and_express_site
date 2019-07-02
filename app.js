70b05b59e440d0c9e8e8ffe2b372a6228710b4de
Micah Hobbs <micahthobbs@gmail.com>
2019-07-02 17:51:17 +1000

    Project templating

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require(`express`);
-// const data = require(`./data.json`);
+const { projects } = require(`./data.json`);
 
 const app = express();
-app.use(express.static(`public`));
+
+app.use(`/static`, express.static(`public`));
 
 app.set('view engine', 'pug');
 
@@ -12,6 +13,17 @@ app.get(`/`, (req, res) => {
 
 app.get(`/about`, (req, res) => {
   res.render(`about`);
+});
+
+app.get(`/project/:id`, (req, res) => {
+  res.render(`project`, {
+    Title: projects[req.params.id].project_name,
+    Description: projects[req.params.id].description,
+    Technologies: projects[req.params.id].technologies,
+    Live_Link: projects[req.params.id].live_link,
+    Github_Link: projects[req.params.id].github_link,
+    Images: projects[req.params.id].image_urls,
+  });
 });
 
 app.listen(3000);
